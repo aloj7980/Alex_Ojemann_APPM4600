ba05d9e7083dd90bb922ee7bffc4468718f43785
Alex Ojemann <82542046+aloj7980@users.noreply.github.com>
2023-10-19 23:58:31 -0600

    Add files via upload

---
+++ Labs/Lab 8/main.py
@@ -0,0 +1,127 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import math
+from numpy.linalg import inv
+
+
+def driver():
+    f = lambda x: 1/(1+(10*x)**2)
+    a = -1
+    b = 1
+
+    ''' create points you want to evaluate at'''
+    Neval = 100
+    xeval = np.linspace(a, b, Neval)
+
+    ''' number of intervals'''
+    Nint = 10
+
+    '''evaluate the linear spline'''
+    yeval = eval_lin_spline(xeval, Neval, a, b, f, Nint)
+
+    '''evaluate the cubic spline'''
+    yeval2 = eval_cubic_spline(xeval, Neval, a, b, f, Nint)
+
+    ''' evaluate f at the evaluation points'''
+    fex = np.zeros(Neval)
+    for j in range(Neval):
+        fex[j] = f(xeval[j])
+
+    plt.figure()
+    plt.plot(xeval, fex, 'ro-')
+    plt.plot(xeval, yeval, 'bs-')
+    plt.legend()
+    plt.show()
+
+    err = abs(yeval - fex)
+    plt.figure()
+    plt.plot(xeval, err, 'ro-')
+    plt.show()
+
+    plt.figure()
+    plt.plot(xeval, fex, 'ro-')
+    plt.plot(xeval, yeval2, 'bs-')
+    plt.legend()
+    plt.show()
+
+    err = abs(yeval2 - fex)
+    plt.figure()
+    plt.plot(xeval, err, 'ro-')
+    plt.show()
+
+
+def eval_lin_spline(xeval, Neval, a, b, f, Nint):
+    '''create the intervals for piecewise approximations'''
+    xint = np.linspace(a, b, Nint + 1)
+
+    '''create vector to store the evaluation of the linear splines'''
+    yeval = np.zeros(Neval)
+
+    for jint in range(Nint):
+        '''find indices of xeval in interval (xint(jint),xint(jint+1))'''
+        '''let ind denote the indices in the intervals'''
+        '''let n denote the length of ind'''
+        ind = np.where(np.logical_and(xeval >= xint[jint] , xeval < xint[jint + 1]))
+        '''temporarily store your info for creating a line in the interval of 
+         interest'''
+        a1 = xint[jint]
+        fa1 = f(a1)
+        b1 = xint[jint + 1]
+        fb1 = f(b1)
+        m = (fb1 - fa1) / (b1 - a1)
+        for kk in range(len(ind)):
+            '''use your line evaluator to evaluate the lines at each of the points 
+            in the interval'''
+
+            '''yeval(ind(kk)) = call your line evaluator at xeval(ind(kk)) with 
+            the points (a1,fa1) and (b1,fb1)'''
+            yeval[ind[kk]] = m*(xeval[ind[kk]] - a1) + fa1
+
+    return yeval
+
+
+
+import numpy as np
+
+
+#This has verry bizarre behavior around 0
+def eval_cubic_spline(xeval, Neval, a, b, f, Nint):
+    xint = np.linspace(a, b, Nint + 1)
+    yeval = np.zeros(Neval)
+
+    for jint in range(Nint):
+        ind = np.where(np.logical_and(xeval >= xint[jint], xeval < xint[jint + 1]))
+
+        a1 = xint[jint]
+        fa1 = f(a1)
+        b1 = xint[jint + 1]
+        fb1 = f(b1)
+
+        h = b1 - a1
+
+        if jint > 0:
+            fprev = f(xint[jint - 1])
+            hprev = a1 - xint[jint - 1]
+            mprev = (fa1 - fprev) / hprev
+        else:
+            mprev = 0
+
+        A = np.array([[h ** 2, h ** 3], [2 * h, 3 * h ** 2]])
+        B = np.array([fb1 - fa1 - mprev * h, (fb1 - fa1) / h - mprev])
+
+        coeffs = np.linalg.solve(A, B)
+
+        C = fa1 / h - (h * coeffs[0]) / 6
+        D = fb1 / h - (h * coeffs[1]) / 6
+
+
+        for kk in range(len(ind)):
+            yeval[ind[kk]] = (((b1 - xeval[ind[kk]])**3)*coeffs[0])/(6*h) + (((xeval[ind[kk]] - a1)**3)*coeffs[1])/(6*h) + C*(b1 - 1) + D*(xeval[ind[kk]] - a1)
+
+    return yeval
+
+
+if __name__ == '__main__':
+    # run the drivers only if this is called from the command line
+    driver()
+
